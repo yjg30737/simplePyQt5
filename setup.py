64f14d97ffa77e3fce2fa00c6f0ead893dc9b41a
yjg30737 <yjg30737@gmail.com>
2021-11-17 18:10:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='simplePyQt5',
     version='0.0.2',
-    author='yjg30737',
+    author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
-    license='BSD',
+    license='FreeBSD',
     packages=['simplePyQt5'],
     description='Make things with PyQt5 easier',
     url='https://github.com/yjg30737/simplePyQt5.git',
