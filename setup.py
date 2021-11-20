e6f9bfbb134938ef4388d89b6eb668db96f5c77a
Jung Gyu Yoon <yjg30737@gmail.com>
2021-11-20 12:33:12 +0900

    correct license from setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.2',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
-    license='FreeBSD',
+    license='MIT',
     packages=['simplePyQt5'],
     description='Make things with PyQt5 easier',
     url='https://github.com/yjg30737/simplePyQt5.git',
